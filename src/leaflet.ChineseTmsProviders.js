6c617388faac62e370822fee3eb3dcbd58d48eb4
LiqunHu <huliquns@126.com>
2020-04-26 13:22:07 +0800

    修改google地图url

--- src/leaflet.ChineseTmsProviders.js
+++ src/leaflet.ChineseTmsProviders.js
@@ -59,7 +59,8 @@ L.TileLayer.ChinaProvider.providers = {
       Map: 'http://mt2.google.cn/vt?lyrs=m@180000000&hl=zh-CN&gl=cn&src=app&x={x}&y={y}&{z}=12&s=Gal'
     },
     Satellite: {
-      Map: 'http://mt1.google.cn/vt?lyrs=y&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gal'
+      // Map: 'http://mt1.google.cn/vt?lyrs=y&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gal'
+      Map: 'https://mt1.google.cn/vt/lyrs=y&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=G'
     },
     placeName: {
       Map: "https://ditu.google.cn/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i413113259!3m12!2szh-CN!3sCN!5e18!12m4!1e68!2m2!1sset!2sRoadmapSatellite!12m3!1e37!2m1!1ssmartmaps!4e0!23i1301875&token=59227"
