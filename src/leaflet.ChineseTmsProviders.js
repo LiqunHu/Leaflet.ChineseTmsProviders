bf2ffb1342d9672b65d61ad503cbe55bf4eb44ae
LiqunHu <huliquns@126.com>
2020-04-26 13:58:01 +0800

    修改图

--- src/leaflet.ChineseTmsProviders.js
+++ src/leaflet.ChineseTmsProviders.js
@@ -56,12 +56,16 @@ L.TileLayer.ChinaProvider.providers = {
 
   Google: {
     Normal: {
-      Map: 'http://mt3.google.cn/vt/lyrs=t@131,r@216000000&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=Gal'
+      // Map: 'http://mt3.google.cn/vt/lyrs=t@131,r@216000000&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=Gal'
+      Map: 'http://mt2.google.cn/vt?lyrs=m@180000000&hl=zh-CN&gl=cn&src=app&x={x}&y={y}&z={z}&s=Gal'
     },
     Satellite: {
       // Map: 'http://mt1.google.cn/vt?lyrs=y&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gal'
       Map: 'https://mt1.google.cn/vt/lyrs=y&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=G'
     },
+    Light: {
+      Map: 'http://mt3.google.cn/vt/lyrs=t@131,r@216000000&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=Gal'
+    }
     placeName: {
       Map: "https://ditu.google.cn/maps/vt?pb=!1m5!1m4!1i{z}!2i{x}!3i{y}!4i256!2m3!1e0!2sm!3i413113259!3m12!2szh-CN!3sCN!5e18!12m4!1e68!2m2!1sset!2sRoadmapSatellite!12m3!1e37!2m1!1ssmartmaps!4e0!23i1301875&token=59227"
     },
